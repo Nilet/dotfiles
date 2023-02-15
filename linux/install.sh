7a24c15e13e325c4e103d0c0c0ee826a1aa44b2f
Matheus Henrique <nilet@tutanota.com>
2023-02-15 08:55:18 -0300

    fixed user stuff

--- linux/install.sh
+++ linux/install.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
 
-if [ $UID -eq 0 ]; then
-        echo 'This script should not be run as root' >&2
-        exit 1
+if [ "$UID" -eq 0 ]; then
+    echo 'This script should not be run as root' >&2
+    exit 1
 fi
 
-sudo echo "Getting sudo ready"
+echo "Getting sudo ready"
+sudo -v
 
-fontList="nerd-fonts-meslo terminus-font-td1 noto-fonts-emoji noto-fonts-cjk" 
+fontList="nerd-fonts-meslo terminus-font noto-fonts-emoji noto-fonts-cjk"
 aurExtras="spotify lf discord-ptb yt-dlp jumpapp"
 
 echo "Moving dotfiles to home"
-sudo cp -r gotoHome/.* ~
+rsync -av gotoHome/ ~
 echo "Moved to home"
+
 echo "Installing packages"
 sudo pacman -Sy
 sudo pacman -S --needed --noconfirm $(cat installList)
@@ -36,8 +38,7 @@ yay -S --needed --noconfirm $aurExtras
 echo "Installed extra software"
 
 echo "Installing zsh plugins"
-sudo chown -R matheus ~
-mkdir ~/.zsh/plugins
+mkdir -p ~/.zsh/plugins
 cd ~/.zsh/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting
@@ -49,6 +50,7 @@ sudo systemctl start sshd.service
 echo "SSH setup finished"
 
 echo "Installing i3currentmedia"
+mkdir -p ~/.local/scripts/
 cd ~/.local/scripts/
 git clone https://github.com/Nilet/i3currentMedia
 echo "i3currentmedia installed"
@@ -56,3 +58,7 @@ echo "i3currentmedia installed"
 echo "Changing zsh to default shell"
 chsh -s /usr/bin/zsh
 echo "ZSH is the default shell now"
+
+sed -i "s/matheus/$(whoami)/g" ~/.config/i3/config
+
+echo "Setup complete"
