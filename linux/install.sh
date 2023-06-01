a868d79a65d5c92bf3b2955666b986ac8d8ff20b
Nilet <nilet@tutanota.com>
2023-06-01 19:53:28 -0300

    A big refactor and added safeguard, hope it works

--- linux/install.sh
+++ linux/install.sh
@@ -1,77 +1,145 @@
 #!/bin/sh
 
+STATUS_FILE="status"
+
+if [ ! -f "$STATUS_FILE" ]; then
+    touch "$STATUS_FILE"
+fi
+
 if [ "$UID" -eq 0 ]; then
     echo 'This script should not be run as root' >&2
     exit 1
 fi
 
+check_status() {
+    STEP_NAME=\$1
+    if grep -q "$STEP_NAME" "$STATUS_FILE"; then
+        echo "Skipping $STEP_NAME"
+        return 0
+    else
+        return 1
+    fi
+}
+
+update_status() {
+    STEP_NAME=\$1
+    echo "$STEP_NAME" >> "$STATUS_FILE"
+}
+
 echo "Getting sudo ready"
 sudo -v
 
-fontList="nerd-fonts-meslo terminus-font noto-fonts-emoji noto-fonts-cjk ttf-terminus-nerd ttf-hack"
-aurExtras="jumpapp insomnia-bin"
+FONTLIST="nerd-fonts-meslo terminus-font noto-fonts-emoji noto-fonts-cjk ttf-terminus-nerd ttf-hack ttf-liberation"
+AUREXTRAS="jumpapp insomnia-bin"
 
-echo "Moving dotfiles to home"
-sudo pacman -S --noconfirm rsync 
-rsync -av gotoHome/ ~
-echo "Moved to home"
+if ! check_status "dotfiles"; then
+    MESSAGE="Moving dotfiles to home"
+    echo $MESSAGE
+    sudo pacman -S --noconfirm rsync || exit 1
+    rsync -av gotoHome/ ~ || exit 1
+    echo "Moved to home"
+    update_status "dotfiles"
+fi
 
-echo "Installing packages"
-sudo pacman -Sy
-sudo pacman -S --needed --noconfirm $(cat installList)
-echo "Packages installed"
+if ! check_status "packages"; then
+    MESSAGE="Installing packages"
+    echo $MESSAGE
+    sudo pacman -Sy || exit 1
+    sudo pacman -S --needed --noconfirm $(cat packageList) || exit 1
+    echo "Packages installed"
+    update_status "packages"
+fi
 
-echo "Installing yay Aur Helper"
-rm -rf yay > /dev/null
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-rm -rf yay > /dev/null
-cd ~
-echo "Yay installed"
+if ! check_status "yay"; then
+    MESSAGE="Installing yay Aur Helper"
+    echo $MESSAGE
+    rm -rf yay > /dev/null
+    git clone https://aur.archlinux.org/yay.git || exit 1
+    cd yay
+    makepkg -si || exit 1
+    rm -rf yay > /dev/null
+    cd ~
+    echo "Yay installed"
+    update_status "yay"
+fi
 
-echo "Installing fonts via AUR"
-yay -Sy
-yay -S --needed --noconfirm $fontList
-echo "Installed fonts via AUR"
+if ! check_status "fonts"; then
+    MESSAGE="Installing fonts via AUR"
+    echo $MESSAGE
+    yay -Sy || exit 1
+    yay -S --needed --noconfirm $FONTLIST || exit 1
+    echo "Installed fonts via AUR"
+    update_status "fonts"
+fi
 
-echo "Installing extras with yay"
-yay -S --needed --noconfirm $aurExtras
-echo "Installed extra software"
+if ! check_status "extras"; then
+    MESSAGE="Installing extras with yay"
+    echo $MESSAGE
+    yay -S --needed --noconfirm $AUREXTRAS || exit 1
+    echo "Installed extra software"
+    update_status "extras"
+fi
 
-echo "Installing zsh plugins"
-mkdir -p ~/.zsh/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions/
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/plugins/fast-syntax-highlighting/
-echo "zsh plugins installed"
+if ! check_status "zsh_plugins"; then
+    MESSAGE="Installing zsh plugins"
+    echo $MESSAGE
+    mkdir -p ~/.zsh/plugins
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions/ || exit 1
+    git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.zsh/plugins/fast-syntax-highlighting/ || exit 1
+    echo "zsh plugins installed"
+    update_status "zsh_plugins"
+fi
 
-echo "tmux time"
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-~/.tmux/plugins/tpm/bin/install_plugins
-echo "tmux finally set up"
+if ! check_status "tmux"; then
+    MESSAGE="Setting up tmux"
+    echo $MESSAGE
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm || exit 1
+    ~/.tmux/plugins/tpm/bin/install_plugins || exit 1
+    echo "tmux finally set up"
+    update_status "tmux"
+fi
 
-echo "Setting up neovim"
-git clone https://github.com/Nilet/nvim ~/.config/nvim
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim -c ":PackerSync"
-echo "Nvim working as usual"
+if ! check_status "neovim"; then
+    MESSAGE="Setting up neovim"
+    echo $MESSAGE
+    git clone https://github.com/Nilet/nvim ~/.config/nvim || exit 1
+    git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+     ~/.local/share/nvim/site/pack/packer/start/packer.nvim || exit 1
+    nvim -c ":PackerSync"
+    echo "Nvim working as usual"
+    update_status "neovim"
+fi
 
-echo "Setting up SSH service"
-sudo systemctl enable sshd.service
-sudo systemctl start sshd.service
-echo "SSH setup finished"
+if ! check_status "ssh"; then
+    MESSAGE="Setting up SSH service"
+    echo $MESSAGE
+    sudo systemctl enable sshd.service || exit 1
+    sudo systemctl start sshd.service || exit 1
+    echo "SSH setup finished"
+    update_status "ssh"
+fi
 
-echo "Installing i3currentmedia"
-mkdir -p ~/.local/scripts/
-git clone https://github.com/Nilet/i3currentMedia ~/.local/scripts/i3currentMedia/
-echo "i3currentmedia installed"
+if ! check_status "i3currentmedia"; then
+    MESSAGE="Installing i3currentmedia"
+    echo $MESSAGE
+    mkdir -p ~/.local/scripts/
+    git clone https://github.com/Nilet/i3currentMedia ~/.local/scripts/i3currentMedia/ || exit 1
+    echo "i3currentmedia installed"
+    update_status "i3currentmedia"
+fi
 
-echo "Changing zsh to default shell"
-chsh -s /usr/bin/zsh
-echo "ZSH is the default shell now"
+if ! check_status "zsh_default"; then
+    MESSAGE="Changing zsh to default"
+    echo $MESSAGE
+    chsh -s /usr/bin/zsh || exit 1
+    echo "ZSH is the default shell now"
+    update_status "zsh_default"
+fi
 
-sed -i "s/matheus/$(whoami)/g" ~/.config/i3/config
-sed -i "s/matheus/$(whoami)/g" ~/.zshrc
+if ! check_status "user_configs"; then
+    sed -i "s/matheus/$(whoami)/g" ~/.config/i3/config
+    sed -i "s/matheus/$(whoami)/g" ~/.zshrc
+    update_status "user_configs"
+fi
 
 echo "Setup complete"
